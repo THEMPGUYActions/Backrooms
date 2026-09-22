8fd01eba16f4e54851fd35e60602c7548b7cd7b3
THEMPGUYActions <THEMPGUYActions@users.noreply.github.com>
2026-09-22 21:16:50 +0000

    deploy: dev 30a0cc58917409bd1a086c9e2f439495b8047ec5

--- dev/src/input.js
+++ dev/src/input.js
@@ -39,7 +39,10 @@ export class InputManager {
     });
 
     this.game.renderer.domElement.addEventListener("click", () => {
-      if(matchMedia("(pointer:fine)").matches && !this.game.paused){const lock=this.game.renderer.domElement.requestPointerLock?.();lock?.catch(()=>{});}
+      if(!touchCapable() && !this.game.paused){
+        const lock=this.game.renderer.domElement.requestPointerLock?.();
+        lock?.catch(()=>{});
+      }
     });
 
     const stick = document.getElementById("stick-zone");
