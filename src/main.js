ed21353ff6ebe7360b3b39d8318a3529c60bbb16
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-22 12:50:11 -0400

    Register persistent asset cache

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { BackroomsGame } from "./game.js";
 
+const registerCache=async()=>{
+  if(!("serviceWorker" in navigator))return;
+  try{
+    const registration=await navigator.serviceWorker.register("../sw.js",{scope:"./"});
+    await navigator.serviceWorker.ready;
+    registration.active?.postMessage({type:"WARM_ASSETS"});
+  }catch(error){
+    console.warn("[Backrooms] Asset cache unavailable:",error);
+  }
+};
+
+registerCache();
 const game = new BackroomsGame();
 game.mount();
 
