b1b63b4c395fb30aa83127e168362eecd04c2fb3
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-22 15:52:40 -0400

    Remove unavailable reference shelf asset

--- scripts/quality.mjs
+++ scripts/quality.mjs
@@ -22,7 +22,7 @@ const SPB_ASSET_FILES = [
   "fluorescent_light.png",
   "wall_trim_texture.png",
   "newstairs_texture.png",
-  "double_sided_shelf.png"
+  "
 ];
 const PBR_ASSET_FILES = [
   "wallpaper_color.png",
