e51d5de1caf794d24b56cc07abfea9a7e75b3600
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-22 15:50:59 -0400

    Defer dev admin until game is loaded

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { BackroomsGame } from "./game.js";
-import { BackroomsAdmin } from "./admin.js";
 
 const registerCache=async()=>{
   if(!("serviceWorker" in navigator))return;
@@ -19,8 +18,15 @@ const registerCache=async()=>{
 };
 
 registerCache();
-const game = new BackroomsGame();
-game.mount();
+const game=new BackroomsGame();
+game.mount().then(async()=>{
+  if(new URLSearchParams(location.search).get("admin")!=="1")return;
+  try{
+    const {BackroomsAdmin}=await import("./admin.js");
+    window.backroomsAdmin=new BackroomsAdmin(game);
+  }catch(error){
+    console.warn("[Backrooms] Admin module unavailable:",error);
+  }
+});
 
-window.backrooms = game;
-window.backroomsAdmin = new BackroomsAdmin(game);
+window.backrooms=game;
