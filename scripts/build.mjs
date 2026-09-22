a8e3288ab395c9981d4421d09118f596c7819279
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-22 15:52:37 -0400

    Remove unavailable reference shelf asset

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -23,7 +23,7 @@ const SPB_FILES = [
   {name:"fluorescent_light.png",path:"src/main/resources/assets/spb-revamped/textures/block/fluorescent_light.png"},
   {name:"wall_trim_texture.png",path:"src/main/resources/assets/spb-revamped/textures/block/wall_trim_texture.png"},
   {name:"newstairs_texture.png",path:"src/main/resources/assets/spb-revamped/textures/block/newstairs_texture.png"},
-  {name:"double_sided_shelf.png",path:"src/main/resources/assets/spb-revamped/textures/block/double_sided_shelf.png"}
+  {name:"
 ];
 
 const lock = JSON.parse(await readFile(LOCK_PATH, "utf8"));
