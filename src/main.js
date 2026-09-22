7991492def42679b30e4314942229e3c4efd5bf6
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-22 13:50:40 -0400

    Add development admin panel loader

--- src/main.js
+++ src/main.js
@@ -3,4 +3,4 @@ import { BackroomsGame } from "./game.js";
 const game = new BackroomsGame();
 game.mount();
 
-window.backrooms = game;
+window.backrooms = game;\n\n// DEV_ADMIN_START\nif(new URLSearchParams(location.search).get("admin")==="1"){\n  import("./admin.js").then(({installAdminPanel})=>installAdminPanel(game));\n}\n// DEV_ADMIN_END
