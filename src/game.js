6a63b7d6826699d7d508cc00801ea8fa4f63c630
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-23 16:09:48 -0400

    Bump Level 0 asset cache key

--- src/game.js
+++ src/game.js
@@ -6,7 +6,7 @@ import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
 import { InputManager } from "./input.js?v=20260923-2050";
 import { AudioDirector } from "./audio.js?v=20260923-2050";
 import { LEVELS, levelById, cycleHash } from "./levels.js?v=20260923-lobbymeta2";
-import { makeLibrary, applyOpenGameArtPBR, applyLevel0Assets, applyLevel1Assets, disposeLibrary, box, makePropSet } from "./assets.js?v=20260923-lobbyassets2";
+import { makeLibrary, applyOpenGameArtPBR, applyLevel0Assets, applyLevel1Assets, disposeLibrary, box, makePropSet } from "./assets.js?v=20260923-lobbyassets3";
 import { level0RotationForMask, level0TransformBlock } from "./level0_templates.js?v=20260923-lobbytemplates3";
 import { LEVEL0_SOURCE_STRUCTURES } from "./level0_source.generated.js?v=20260923-l0source1";
 
