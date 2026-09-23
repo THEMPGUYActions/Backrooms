59708e6e505b3407bb352febc030a7aedb3cce19
THEMPGUYActions <THEMPGUYActions@users.noreply.github.com>
2026-09-23 12:45:38 +0000

    deploy: dev 351b7097ab14bbd68416ae30b434e2bb16e52543

--- dev/src/main.js
+++ dev/src/main.js
@@ -1,4 +1,4 @@
-import { BackroomsGame } from "./game.js?v=20260923-1252";
+import { BackroomsGame } from "./game.js?v=20260923-1304";
 
 const registerCache=async()=>{
   if(!("serviceWorker" in navigator))return;
@@ -23,7 +23,7 @@ game.mount().then(async()=>{
   /* DEV_ADMIN_START */
   if(new URLSearchParams(location.search).get("admin")!=="1")return;
   try{
-    const {BackroomsAdmin}=await import("./admin.js?v=20260923-1205");
+    const {BackroomsAdmin}=await import("./admin.js?v=20260923-1304");
     window.backroomsAdmin=new BackroomsAdmin(game);
     window.backroomsAdmin.activate();
   }catch(error){
