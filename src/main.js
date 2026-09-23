8dfd08c4ff1effdf4b3d57ce171b637acde8ea58
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-23 08:38:47 -0400

    Refresh game runtime cache

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { BackroomsGame } from "./game.js?v=20260923-1252";
+import { BackroomsGame } from "./game.js?v=20260923-1304";
 
 const registerCache=async()=>{
   if(!("serviceWorker" in navigator))return;
