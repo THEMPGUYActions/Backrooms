20bdc71d4f119cb76800950c30efa7042ae5346b
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-23 16:17:52 -0400

    Bump Level 0 source module cache key

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { BackroomsGame } from "./game.js?v=20260923-l0source1";
+import { BackroomsGame } from "./game.js?v=20260923-l0source2";
 /* DEV_ADMIN_START */
 import { BackroomsAdmin } from "./admin.js?v=20260923-1950";
 /* DEV_ADMIN_END */
