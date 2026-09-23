7c68e5d856c26e1450acdf851e62e28fae6fafc3
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-23 17:06:27 -0400

    Refresh expanded Level 0 module

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { BackroomsGame } from "./game.js?v=20260923-l0wiki2";
+import { BackroomsGame } from "./game.js?v=20260923-l0wiki3";
 /* DEV_ADMIN_START */
 import { BackroomsAdmin } from "./admin.js?v=20260923-1950";
 /* DEV_ADMIN_END */
