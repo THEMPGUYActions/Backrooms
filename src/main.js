3fd649b9c3bc8fd5ad67e76f96d8d799895db87b
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-23 16:47:58 -0400

    Bump game module cache version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { BackroomsGame } from "./game.js?v=20260923-l0seed6";
+import { BackroomsGame } from "./game.js?v=20260923-l0seed7";
 /* DEV_ADMIN_START */
 import { BackroomsAdmin } from "./admin.js?v=20260923-1950";
 /* DEV_ADMIN_END */
