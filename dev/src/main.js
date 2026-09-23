feda1f3141378c2137c187d8a1d49c2a172ba7f0
THEMPGUYActions <THEMPGUYActions@users.noreply.github.com>
2026-09-23 13:06:28 +0000

    deploy: dev 16dd41e6a7ea19e766e0494197959d13a981273a

--- dev/src/main.js
+++ dev/src/main.js
@@ -1,6 +1,6 @@
-import { BackroomsGame } from "./game.js?v=20260923-1600";
+import { BackroomsGame } from "./game.js?v=20260923-1830";
 /* DEV_ADMIN_START */
-import { BackroomsAdmin } from "./admin.js?v=20260923-1600";
+import { BackroomsAdmin } from "./admin.js?v=20260923-1830";
 /* DEV_ADMIN_END */
 
 const registerCache=async()=>{
