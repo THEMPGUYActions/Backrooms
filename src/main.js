0a1bca22015536425c6596f90fd87210c0bb83d5
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-26 13:38:34 -0400

    Bust red room asset cache

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { BackroomsGame } from "./game.js?v=20260926-redzone2";
+import { BackroomsGame } from "./game.js?v=20260926-redzone3";
 import { installBackroomsFixes } from "./fixes.js?v=20260926-flashlight2";
 /* DEV_ADMIN_START */
 import { BackroomsAdmin } from "./admin.js?v=20260925-entityai2";
