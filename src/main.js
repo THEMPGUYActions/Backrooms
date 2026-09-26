63904e7e3321b59d3f26fc3cf58d303d28e83fff
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-26 12:35:16 -0400

    Refresh Level 0 floor and ceiling seam fixes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { BackroomsGame } from "./game.js?v=20260926-manilalayout4";
-import { installBackroomsFixes } from "./fixes.js?v=20260926-roofmanila2";
+import { installBackroomsFixes } from "./fixes.js?v=20260926-roofmanila3";
 /* DEV_ADMIN_START */
 import { BackroomsAdmin } from "./admin.js?v=20260925-entityai2";
 /* DEV_ADMIN_END */
