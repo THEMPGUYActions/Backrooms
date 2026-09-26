3abf99ca82cd25127c018833b3e3fdfd74602f34
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-26 14:33:21 -0400

    Bust flashlight cache for Red Zone boost

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { BackroomsGame } from "./game.js?v=20260926-redzone-audio5";
+import { BackroomsGame } from "./game.js?v=20260926-redzone-flash1";
 import { installBackroomsFixes } from "./fixes.js?v=20260926-flashlight2";
 /* DEV_ADMIN_START */
 import { BackroomsAdmin } from "./admin.js?v=20260925-entityai2";
