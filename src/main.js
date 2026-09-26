b42796737b655a4e1a471dee012c3ac221d5f10d
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-25 21:03:45 -0400

    Bump entity AI cache version

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { BackroomsGame } from "./game.js?v=20260925-entities1";
+import { BackroomsGame } from "./game.js?v=20260925-entityai2";
 /* DEV_ADMIN_START */
-import { BackroomsAdmin } from "./admin.js?v=20260925-entities1";
+import { BackroomsAdmin } from "./admin.js?v=20260925-entityai2";
 /* DEV_ADMIN_END */
 
 const registerCache=async()=>{
